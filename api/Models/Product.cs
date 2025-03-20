ad0f99ebc12ae40d0ca5cfccb1fcf40fa5de8050
Mahimna M Mehta <65457672+mahimnamehta@users.noreply.github.com>
2025-03-20 05:40:48 -0400

    Create Product Model
    Created Product Model
    Ran migrations
    Updated the migrations to the database file

---
+++ api/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        public string? Description { get; set; }  // Optional field
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int StockQuantity { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
